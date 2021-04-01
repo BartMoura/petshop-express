ba774fda1e675471e955c3a48cc26d8ca209691a
BartMoura <bart.emf@hotmail.com>
2021-04-01 13:52:29 -0300

    listarPets refatorada

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const petshop = require('./petshop');
+
 const app = express();
 
 app.listen(3000, () => {
     console.log('Servidor rodando!');
 });
+
+console.log(petshop.listarPets());
